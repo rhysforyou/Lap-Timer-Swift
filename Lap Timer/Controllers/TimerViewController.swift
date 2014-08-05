1be44916ba6aca1d49bb6c74d0ee9a8bb4882e77
Rhys Powell <rhys@rpowell.me>
2014-08-05 18:09:09 +1000

    Created view controllers

---
+++ Lap Timer/Controllers/TimerViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TimerViewController.swift
+//  Lap Timer
+//
+//  Created by Rhys Powell on 5/08/2014.
+//  Copyright (c) 2014 Rhys Powell. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+	@IBAction func unwindToTiemrViewController(segue: UIStoryboardSegue) {
+		
+	}
+}
